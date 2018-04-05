17c975d431814738c41277a3601b378a909438bb
Baptiste Mayelle <baptiste.mayelle@cdbdx.biz>
2018-04-05 15:32:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Gorm ForeignKey
+Gorm don't manage the foreign key by default.
+
+This package can. Basically, but it can do it.
+
+Some things needs to be done to be perfectly compatible with gorm ; like using an another column name for the foreign key etc.
+
+# How to use it
+You can define a new foreign key for this kind of relation : 
+
+* Belongs To ;
+* Many to Many.
+
+Some will be coming in the future.
+
+## Belongs To
+After the automigrate functions, you can do this :
+
+```	
+import (
+    'github.com/youkoulayley/gormfk'
+)
+
+...
+
+bootstrap.Db().AutoMigrate(&models.Role{})
+bootstrap.Db().AutoMigrate(&models.User{})
+
+gormfk.BelongsToFIndex(bootstrap.Db(), &models.User{}, &models.Role{})
+```
+
+## Many To Many
+After the automigrate functions, you can do this :
+
+```
+import (
+    'github.com/youkoulayley/gormfk'
+)
+
+...
+
+bootstrap.Db().AutoMigrate(&models.User{})
+bootstrap.Db().AutoMigrate(&models.Channel{})
+
+gormfk.Many2ManyFIndex(bootstrap.Db(), &models.Channel{}, &models.User{})
+```
---
+++ foreign_key.go
@@ -0,0 +1,54 @@
+package foreignKey
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/inflection"
+)
+
+// BelongsToFIndex create the foreign key for a belongs to relation
+func BelongsToFIndex(db *gorm.DB, parentModel interface{}, childModel interface{}) {
+	// Get name of the model
+	tableParentAccessor := ReduceModelToName(parentModel)
+	tableChildAccessor := ReduceModelToName(childModel)
+
+	// Set model name to plural
+	tableParentName := inflection.Plural(tableParentAccessor)
+	tableChildName := inflection.Plural(tableChildAccessor)
+
+	db.Table(tableParentName).AddForeignKey(tableChildAccessor+"_id", tableChildName+"(id)", "CASCADE", "CASCADE")
+}
+
+// Many2ManyFIndex create the index for many to many relations
+func Many2ManyFIndex(db *gorm.DB, parentModel interface{}, childModel interface{}) {
+	// Get name of the model
+	tableParentAccessor := ReduceModelToName(parentModel)
+	tableChildAccessor := ReduceModelToName(childModel)
+
+	// Set model name to plural
+	tableParentName := inflection.Plural(tableParentAccessor)
+	tableChildName := inflection.Plural(tableChildAccessor)
+
+	// Join the tables
+	joinTable := fmt.Sprintf("%s_%s", tableParentAccessor, tableChildAccessor)
+
+	// Add foreign Key and unique index
+	db.Table(joinTable).AddForeignKey(tableParentAccessor+"_id", tableParentName+"(id)", "CASCADE", "CASCADE")
+	db.Table(joinTable).AddForeignKey(tableChildAccessor+"_id", tableChildName+"(id)", "CASCADE", "CASCADE")
+	db.Table(joinTable).AddUniqueIndex(joinTable+"_unique", tableParentAccessor+"_id", tableChildAccessor+"_id")
+}
+
+// ReduceModelToName get the name of the model by its reference
+func ReduceModelToName(model interface{}) string {
+	value := reflect.ValueOf(model)
+	if value.Kind() != reflect.Ptr {
+		return ""
+	}
+
+	elem := value.Elem()
+	t := elem.Type()
+	rawName := t.Name()
+	return gorm.ToDBName(rawName)
+}
